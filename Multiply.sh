9a614c049110f117148677ef4ddb83351b661ad3
Ami Desai <115481492+AmiDesai11@users.noreply.github.com>
2023-08-17 17:57:46 +0530

    Multiply.sh

--- Multiply.sh
+++ Multiply.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#Purpose: eval command Evaluating twice
-#Version:1.0
-#Created Date: Wed Jun 13 22:09:59 IST 2018
-#Modified Date:
-#WebSite: https://arkit.co.in
-#Author: Ankam Ravi Kumar
-# START #
 
 echo "multification of X*Y"
 echo "Enter X"
@@ -13,5 +6,3 @@ read X
 echo "Enter Y"
 read Y
 echo "X*Y = $X*$Y = $[ X*Y ]"
-
-# END #
